ae51235de96dbd30e7e5d4bc63e98586fe68636c
sachi_2162 <sharmasachidanand1111@gmail.com>
2025-04-30 17:14:17 +0530

    Completed HTML and CSS Series with projects.

---
+++ project4/script.js
@@ -0,0 +1,25 @@
+var rect=document.querySelector('#center');
+
+rect.addEventListener("mousemove",function(details){
+    var rectanglelocation=rect.getBoundingClientRect();
+    var inside=details.clientX-rectanglelocation.left;
+    if(inside<rectanglelocation.width/2){
+        var red=gsap.utils.mapRange(0,rectanglelocation.width/2,255,0,inside);
+        gsap.to(rect,{
+            backgroundColor:`rgb(${red},0,0)`,
+            ease:Power4,
+        });
+    }
+    else{
+        var blue=gsap.utils.mapRange(rectanglelocation.width/2,rectanglelocation.width,0,255,inside);
+        gsap.to(rect,{
+            backgroundColor:`rgb(0,0,${blue})`,
+            ease:Power4,
+        });
+    }
+});
+rect.addEventListener("mouseleave",function(){
+    gsap.to(rect,{
+        backgroundColor: "white",
+    })
+})
